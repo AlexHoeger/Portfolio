e8cd4cfbc6e7b7fca323a1172c9b7d0a920fbb80
Alex H <alex.m.hoeger@gmail.com>
2019-10-31 19:58:29 -0400

    implemented show functionality for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -26,7 +26,7 @@ def create
 
    def update
     @portfolio_item = Portfolio.find(params[:id])
-    
+
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html { redirect_to portfolios_path, notice: 'Your Portfolio was successfully updated.' }
@@ -36,5 +36,9 @@ def create
       end
     end
   end 
+
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 
 end
