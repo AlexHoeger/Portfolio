8db9b375c4f3a2282d9f3f51dccdebf25b006a0e
Alex H <alex.m.hoeger@gmail.com>
2019-10-31 20:48:02 -0400

    implemented destroy feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -41,4 +41,13 @@ def create
     @portfolio_item = Portfolio.find(params[:id])
   end
 
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Item was deleted' }
+    end
+  end
+
 end
